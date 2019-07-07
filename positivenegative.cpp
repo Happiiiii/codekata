dfbd95f94935a2b2349623109820c20d330699e7
Happiiiii <52240799+Happiiiii@users.noreply.github.com>
2019-07-07 18:30:22 +0530

    Create positivenegative.cpp

---
+++ positivenegative.cpp
@@ -0,0 +1,30 @@
+
+
+#include <iostream.h>
+
+
+using namespace std;
+
+
+int main()
+
+{
+    
+int a;
+  
+cin>>a;
+ 
+if(a==0)
+  
+cout<<"zero";
+
+else if(a>0)
+cout<<"positive";
+
+else
+
+cout<<"negative";
+  
+  return 0;
+
+}
