68e306978972071688e6bef5d2795ae3bee81029
Happiiiii <52240799+Happiiiii@users.noreply.github.com>
2019-07-08 15:48:02 +0530

    Create stringReverse.cpp

---
+++ stringReverse.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream> 
+using namespace std; 
+void recursiveReverse(string &str, int i = 0) 
+{ 
+	int n = str.length(); 
+	if (i == n / 2) 
+		return; 
+	swap(str[i], str[n - i - 1]); 
+	recursiveReverse(str, i + 1); 
+} 
+int main() 
+{ 
+	string str = "Hello"; 
+	recursiveReverse(str); 
+	cout << str; 
+	return 0; 
+} 
