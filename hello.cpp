36976c1dfa2e37201a5e5df1f9222017ae1187d9
Happiiiii <52240799+Happiiiii@users.noreply.github.com>
2019-07-09 10:33:49 +0530

    Update and rename helloword.cpp to hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 int main() { 
   int n,i;
-  cout<<"how many times? ";
   cin>>n;
   for(i=0;i<n;i++)
- cout << " HelloWorld " << endl;
+ cout << "Hello"<<endl;
    return 0; 
 }
