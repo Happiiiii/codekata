4c8f938ee2cde7acec2b8738fe5566bb3a0bf178
Happiiiii <52240799+Happiiiii@users.noreply.github.com>
2019-07-09 16:33:14 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main()
 {
 int origNum, num, rem, sum = 0;
-cout << "Enter number: ";
 cin >> origNum;
 num = origNum;
 while(num != 0)
@@ -13,8 +12,8 @@ sum += rem * rem * rem;
 num = num/10;
 }
 if(sum == origNum)
-cout << origNum <<" "<< "yes";
+cout<< "yes";
 else
-cout << origNum <<" "<< "no";
+cout <<"no";
 return 0;
 }
