ab8530e0fc6a67abdece9c117c08f31748f19ea3
Happiiiii <52240799+Happiiiii@users.noreply.github.com>
2019-07-09 11:47:29 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 int main(){
 int num,con = true;
-cout<<"Enter any number: ";
 cin>>num;
 for(int i = 2; i <= num / 2; i++) {
 if(num % i == 0) {
@@ -11,8 +10,8 @@ break;
 }
 }
 if (con==true)
-cout<<num<<" "<<"yes";
+cout<<"yes";
 else
-cout<<num<<" "<<"not";
+cout<<"no";
 return 0;
 }
