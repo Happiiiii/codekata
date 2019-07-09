6fc8fb45bc7c9fa8665e3639997e8c436758fbbc
Happiiiii <52240799+Happiiiii@users.noreply.github.com>
2019-07-09 11:12:10 +0530

    Update exponent.cpp

--- exponent.cpp
+++ exponent.cpp
@@ -3,11 +3,7 @@ using namespace std;
 int main() 
 {
    int num,exponent,result=1;
-   cout<<"enter num: ";
-   cin>>num;
-   cout<<"exponent: ";
-   cin>>exponent;
-   cout<< num << "^" << exponent<< "=";
+   cin>>num>>exponent;
    while(exponent != 0){
    result *= num;
    --exponent;
