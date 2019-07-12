52531a8020ca54d71d20bc58501005e5851d720f
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-07-12 15:46:36 +0530

    Update arraySort.cpp

--- arraySort.cpp
+++ arraySort.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int num,temp;
+	int num;
 	cin>>num;
 	int arr[num];
 	for(int i=0;i<num;i++)
 	{
 		cin>>arr[i];
 	}
+	int temp;
 	for(int i=0;i<num;i++)
 	{
 		for(int j=i+1;j<num;j++) //i+1=> increment of index value
