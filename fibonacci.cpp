7c76619aa279d5992b21552ac8008515908d1438
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-06 00:57:25 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,7 +1,7 @@
     #include <iostream>  
     using namespace std;  
     int main() {  
-      int n1=0,n2=1,n3,i,num;    
+      int n1=1,n2=1,n3,i,num;    
      cin>>num;    
      cout<<n1<<" "<<n2<<" "; 
      for(i=2;i<num;++i) 
