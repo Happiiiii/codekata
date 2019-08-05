47fc0df8a4c1af8e5b82586fd9960820cadc8a13
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-05 22:56:08 +0530

    Create spaceCount.cpp

---
+++ spaceCount.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char str[1000];
+int i,count=0;
+cin.getline(str,1000);
+for(i=0;str[i]!='\0';i++)
+{
+    
+    if(str[i]==' ')
+    count++;
+}
+cout<<count;
+	return 0;
+} 
