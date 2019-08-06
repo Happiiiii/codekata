b6b5ea6cd0e982e46d512b06b5e8317e58d37059
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-06 07:45:37 +0530

    Create stringRepeat.cpp

---
+++ stringRepeat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+   int num;
+   string str;
+   cin>>str>>num;
+   for(int i=0;i<num;i++)
+   {
+       cout << str << "\n";
+   }
+   
+    return 0;
+}
