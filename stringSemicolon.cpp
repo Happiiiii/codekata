a90204635e92514bf0c757c0c16d76c80f48f7da
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-06 23:38:48 +0530

    Create stringSemicolon.cpp

---
+++ stringSemicolon.cpp
@@ -0,0 +1,27 @@
+/*
+
+
+65 - Given a string S, print it without using semicolon in your program.
+Sample Testcase :
+INPUT
+hello world
+OUTPUT
+hello world 
+
+
+
+*/
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string str;
+  getline(cin,str);
+  if(cout<<str)
+  {}
+  return 0;
+}
