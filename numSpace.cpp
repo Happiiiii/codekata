b64300a50461765a29bb8a39b7112384203249ee
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-06 19:27:29 +0530

    Update numSpace.cpp

--- numSpace.cpp
+++ numSpace.cpp
@@ -9,29 +9,23 @@ OUTPUT
 */
 
 #include <iostream>
-
 using namespace std;
-
-int main()
+int main() 
 {
-  int num, rev = 0;
-  int flag = 0;
-  
-  cin >> num;
-  
-  while(num != 0)
-  {
-    rev = (rev*10) + (num%10);
-    num = num/ 10;
-  }
-  
-  while(rev != 0)
-  {
-    if(flag == 1)
-      cout << " ";
-    cout << rev % 10 ;
-    rev = rev/ 10;
-    flag = 1;
-  }
-  return 0;
+   
+   int n,sum=0,split;
+   cin>>n;
+   while(n>0)
+   {
+       split=n%10;
+       sum=sum*10+split;
+       n=n/10;
+   }
+   while(sum>0)
+   {
+       split=sum%10;
+        cout<<split<<" ";
+        sum=sum/10;
+   }
+   return 0;
 }
