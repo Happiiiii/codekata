a6532263f71de560203163b19c742bb82767650f
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-06 12:19:39 +0530

    Create minMaxArrayNum.cpp

---
+++ minMaxArrayNum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int num;
+  cin>>num;
+  int arr[num];
+  for(int i=0;i<num;i++)
+  {
+      cin>>arr[i];
+  }
+  int min=arr[0];
+  for(int i=0;i<num;i++)
+  {
+  if(min>arr[i]) 
+  {
+      min=arr[i];
+  }
+  }
+  int max=arr[0];
+  for(int i=0;i<num;i++)
+  {
+      if(max<arr[i])
+      {
+          max=arr[i];
+      }
+  }
+  cout<<min<<' '<<max;
+  return 0;
+}
