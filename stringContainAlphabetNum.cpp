27139682fd13c2d5c7b05a321dcc1abff43ec9e8
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-06 18:38:11 +0530

    Update stringContainAlphabetNum.cpp

--- stringContainAlphabetNum.cpp
+++ stringContainAlphabetNum.cpp
@@ -9,3 +9,44 @@ Helloworld.123
 OUTPUT
 Yes 
 */
+        /******************************************************************************
+        
+                                      Online C++ Compiler.
+                       Code, Compile, Run and Debug C++ program online.
+        Write your code in this editor and press "Run" button to compile and execute it.
+        
+        *******************************************************************************/
+        
+        #include <iostream>
+        #include<string>
+        using namespace std;
+        
+        int main()
+        {
+            string str;
+            int flag = 1;
+            
+            getline(cin, str);
+            
+            for(int i=0;i<str.length();i++)
+            {
+                if(isdigit(str[i]) || isalpha(str[i]) || str[i] == ' ')
+                {
+                    flag = 1;
+                }
+                else
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            
+            if(flag == 1)
+            {
+                cout << "Yes";
+            }
+            else
+                cout << "No";
+            return 0;
+        }
+
