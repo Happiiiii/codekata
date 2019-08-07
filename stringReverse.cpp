3df95869a98917fcc5184dc2ec01a8eb83e30e42
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-08 00:01:33 +0530

    Update stringReverse.cpp

--- stringReverse.cpp
+++ stringReverse.cpp
@@ -1,18 +1,15 @@
 
-#include <iostream> 
-using namespace std; 
-void recursiveReverse(string &str, int i = 0) 
-{ 
-	int n = str.length(); 
-	if (i == n / 2) 
-		return; 
-	swap(str[i], str[n - i - 1]); 
-	recursiveReverse(str, i + 1); 
-} 
-int main() 
-{ 
-	string str = "Hello"; 
-	recursiveReverse(str); 
-	cout << str; 
-	return 0; 
-} 
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+ string str;
+ cin>>str;
+ for(int i=str.length();i>=0;i--)
+ {
+     cout<<str[i];
+ }
+    return 0;
+}
