d5e2a1613632ac487cdc42ca9e49b701373689b7
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-23 10:57:38 +0530

    Update alpha_numeric_string.cpp

--- alpha_numeric_string.cpp
+++ alpha_numeric_string.cpp
@@ -26,3 +26,30 @@ int main()
   }
   return 0;
 }
+
+/*
+Alpha Numeric String
+abc123d
+abcd
+*/
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  string str;
+  cin >> str;
+  for( int i = 0; str[i]>0; i++ )
+  {
+      if( str[i] >= '0' && str[i] <= '9')
+      {
+        cout << "";
+      }
+      else
+      {
+          cout << str[i];
+      }
+  }
+  return 0;
+}
