87db84d67ae7d32f058bd8fce8a2a0ff524f97b7
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-28 15:49:14 +0530

    Update arrayMidElement.cpp

--- arrayMidElement.cpp
+++ arrayMidElement.cpp
@@ -1,28 +1,59 @@
+/*
+3 
+2 1 3                                                                                                                        
+1 2 3                                                                                                                          
+2                                                                                                                              
+ 
+ 4
+ 1 2 3 4
+ 2 3
+*/
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int num;
-    cin>>num;
-    int arr[num];
-    for(int i=0;i<num;i++)
-    {
-        cin>>arr[i];
-    }
-    int temp;
-    for(int i=0;i<num;i++)
-    {
-       for(int j=i+1;j<num;j++) 
-       {
-           if(arr[i]>arr[j]) //swap
-           {
-               temp=arr[i];
-               arr[i]=arr[j];
-               arr[j]=temp;
-           }
-       }     
-    }
-    num=(num+1)/2-1;
-    cout<<arr[num];
-  return 0;
+  int temp,i,j,fnd;
+  int num;
+  cin>>num;
+  int arr[num];
+  for(i=0;i<num;i++)
+  {
+    cin >> arr[i];  
+  }
+  for(i=0;i<num;i++)
+  {
+      for(j=0;j<num;j++)
+      {
+          if(arr[i] < arr[j] )// sort
+          {
+              temp=arr[i];
+              arr[i]=arr[j];
+              arr[j]=temp;
+          }
+      }
+  }  
+  
+  for(i=0;i<num;i++) //ascending
+  {
+      cout<<arr[i]<<" ";
+  }
+  if(num%2!=0)
+  {
+      fnd=(num/2)+1; //odd
+      for(i=fnd-1;i<fnd;i++)
+      {
+          cout<<"\n"<<arr[i];
+      }
+  }
+  else
+  {
+      fnd=(num/2);  //even
+      for(i=fnd-1;i<=fnd;i++)
+      {
+          cout<<arr[i];
+      }
+  }
+    return 0;
 }
